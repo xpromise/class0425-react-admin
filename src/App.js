f3e1e58b554842de185b4f72257f95b51b4463ee
xiongjian <webjsforyou@163.com>
2019-08-13 14:47:54 +0800

    实现login静态组件

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Admin from './pages/admin';
 import Login from './pages/login';
 
+import './App.css';
+
 export default class App extends Component {
   render() {
     return <Router>
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
+import { Form, Input, Icon, Button } from 'antd';
+import logo from './logo.png';
+
+import './index.less';
+// 定义变量，不能写在import上面，会报eslint的错误
+const Item = Form.Item;
 
 export default class Login extends Component {
   render() {
-    return <div>
-      Login
+    return <div className="login">
+      <header className="login-header">
+        <img src={logo} alt="logo"/>
+        <h1>React项目: 后台管理系统</h1>
+      </header>
+      <section className="login-section">
+        <h2>用户登录</h2>
+        <Form onSubmit={this.login}>
+          <Item>
+            <Input prefix={<Icon type="user" />} placeholder="用户名"/>
+          </Item>
+          <Item>
+            <Input prefix={<Icon type="lock" />} placeholder="密码"/>
+          </Item>
+          <Item>
+            <Button type="primary" htmlType="submit" className="login-btn">登录</Button>
+          </Item>
+        </Form>
+      </section>
     </div>;
   }
 }
